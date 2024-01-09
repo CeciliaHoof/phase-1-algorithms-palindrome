69dc3de13db0a1584c6f26b8a697e17b7d1abe9c
Cecilia <ccchimento@gmail.com>
2024-01-09 15:06:11 -0700

    completed algorithm

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  const length = word.length;
+  for (let x=0; x < (length/2); x++){
+    if (word.charAt(x) === word.charAt(length - (x+1))){
+      return true;
+    }
+    else {return false}
+  }
 }
 
 /* 
   Add your pseudocode here
+  returns true if the string is the same backwards and forwards
+
+  get length of string (length = string.length())
+    for (x= 0; x < length/2; x++)
+    --> is charAt(x) === to charAt(length -(x+1))
+  
+  else (for example tacocat)
+
+    is charAt(0) === charAt(length - 1)?
+    --> is charAt(1) === charAt (length -2)
+    --> 
 */
 
 /*
@@ -14,7 +32,7 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("noon"));
 
   console.log("");
 
